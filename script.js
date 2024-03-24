89e588d0d492e7def414c226a2ff8459c80df002
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-24 22:33:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,90 @@
-//complete this code
-class Animal {
-	constructor(species) {
-		this._species = species;
-	}
+// //complete this code
+// class Animal {
+// 	constructor(species) {
+// 		this._species = species;
+// 	}
 
-	get species() {
-		return this._species;
-	}
+// 	get species() {
+// 		return this._species;
+// 	}
 
-	makeSound() {
-		console.log(`The ${this._species} makes a sound`);
-	}
-}
+// 	makeSound() {
+// 		console.log(`The ${this._species} makes a sound`);
+// 	}
+// }
 
-class Dog extends Animal {
-	constructor(species) {
-		super(species);
-	}
+// class Dog extends Animal {
+// 	constructor(species) {
+// 		super(species);
+// 	}
 	
-	bark() {
-		super.makeSound("woof");
-	}
+// 	bark() {
+// 		super.makeSound("woof");
+// 	}
+// }
+
+// class Cat extends Animal {
+// 	constructor(species) {
+// 		super(species);
+// 	}
+	
+// 	purr() {
+// 		super.makeSound("purr");
+// 	}
+// }
+
+// const myCat = new Cat("Siamese");
+// myCat.makeSound();
+// myCat.purr();
+
+
+// const myDog = new Dog("Golden Retriever");
+// myDog.makeSound();
+// myDog.bark();
+
+// // // Do not change the code below this line
+// // window.Animal = Animal;
+// // window.Dog = Dog;
+// // window.Cat = Cat;
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound(sound) {
+    console.log(`The ${this._species} makes a ${sound}`);
+  }
 }
 
 class Cat extends Animal {
-	constructor(species) {
-		super(species);
-	}
-	
-	purr() {
-		super.makeSound("purr");
-	}
+  constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    super.makeSound("purr");
+  }
 }
 
-const myCat = new Cat("Siamese");
-myCat.makeSound();
-myCat.purr();
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
 
+  bark() {
+    super.makeSound("woof");
+  }
+}
+
+// Example usage
+const myCat = new Cat("Siamese");
+myCat.makeSound("meow"); // Output: The Siamese makes a meow
+myCat.purr(); // Output: The Siamese makes a purr
 
 const myDog = new Dog("Golden Retriever");
-myDog.makeSound();
-myDog.bark();
-
-// // Do not change the code below this line
-// window.Animal = Animal;
-// window.Dog = Dog;
-// window.Cat = Cat;
+myDog.makeSound("bark"); // Output: The Golden Retriever makes a bark
+myDog.bark(); // Output: The Golden Retriever makes a woof

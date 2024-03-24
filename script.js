5bc740d310cb0701434f269fb10bda1043d9dde4
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-24 21:55:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,78 +1,40 @@
+//complete this code
 class Animal {
-  constructor(species) {
-    this._species = species;
-  }
+	constructor(species) {
+		this._species = species;
+	}
 
-  get species() {
-    return this._species;
-  }
+	get species() {
+		return this._species;
+	}
 
-  makeSound() {
-    console.log(`The ${this._species} makes a sound`);
-  }
-}
-
-class Cat extends Animal {
-  purr() {
-    console.log("purr");
-  }
+	makeSound() {
+		console.log(`The ${this._species} makes a sound`);
+	}
 }
 
 class Dog extends Animal {
-  bark() {
-    console.log("woof");
-  }
+	bark() {
+		console.log("woof");
+	}
 }
 
-// Example usage
+class Cat extends Animal {
+	purr() {
+		console.log("purr");
+	}
+}
+
 const myCat = new Cat("Siamese");
-myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr(); // Output: purr
+myCat.makeSound();
+myCat.purr();
+
 
 const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark(); // Output: woof
+myDog.makeSound();
+myDog.bark();
 
-
-
-
-// //complete this code
-// class Animal {
-// 	constructor(species) {
-// 		this.species = species;
-// 	}
-
-// 	get species() {
-// 		return this.species;
-// 	}
-
-// 	makeSound() {
-// 		console.log(`The ${this.species} makes a sound`);
-// 	}
-// }
-
-// class Dog extends Animal {
-// 	bark() {
-// 		console.log("woof");
-// 	}
-// }
-
-// class Cat extends Animal {
-// 	purr() {
-// 		console.log("purr");
-// 	}
-// }
-
-// const myCat = new Cat("Siamese");
-// myCat.makeSound();
-// myCat.purr();
-
-
-// const myDog = new Dog("Golden Retriever");
-// myDog.makeSound();
-// myDog.bark();
-
-// // // Do not change the code below this line
-// // window.Animal = Animal;
-// // window.Dog = Dog;
-// // window.Cat = Cat;
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
